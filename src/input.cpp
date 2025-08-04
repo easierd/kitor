4cff75971382de1993753da81bd8fb709e367756
david_ferrari <ferraridavid.hub@gmail.com>
2025-08-04 12:04:19 +0200

    Refactor

---
+++ src/input.cpp
@@ -0,0 +1,4 @@
+enum class Input {
+    CTRL_C = 3,
+    DEL = 127,
+};
--- src/kitor.cpp
+++ src/kitor.cpp
@@ -1,36 +1,42 @@
 #include "kitor.h"
+#include "input.cpp"
 
 #include <iostream>
 
 
 Kitor::Kitor(const std::string& filename) : 
                 text {""},
-                os{std::ofstream{filename}} {}
+                os{std::ofstream{filename}} {
+    
+    terminal.save_state();
+    terminal.clear();
+}
 
 
 void Kitor::run() {
-    terminal.save_state();
-    terminal.clear();
-
     char c;
-    while(std:: cin.get(c)) {
-        if (c == 3) {   // Ctrl-C
-            break;
-        }
+    bool is_terminated = false;
+    while((!is_terminated) && (std::cin.get(c))) {
 
-        if (c == 127) { // Del
-            terminal.delete_last();
+        auto input = static_cast<Input>(c);
+        switch (input) {
+            case Input::CTRL_C: 
+                is_terminated = true;
+                break;
 
-            if (!text.empty()) {
-                text.pop_back();
-            }
-            terminal.refresh(text);
+            case Input::DEL: 
+                terminal.delete_last();
+                if (!text.empty()) {
+                    text.pop_back();
+                }
+                terminal.refresh(text);
+                break;
             
-            continue;
+            default:
+                text.push_back(c);
+                terminal.refresh(text);
+                break;
         }
-
-        text.push_back(c);
-        terminal.refresh(text);
     }
 }
 
